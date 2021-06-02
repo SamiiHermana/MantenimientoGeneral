416c65f470ce80bb815c291e5bd8b4d3498b6f10
shermana <shermana@holos.cl>
2021-06-02 19:41:57 -0300

    [Javascript] Uso prompt, alert y console.log

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let nombre= (prompt("Ingrese su Nombre"));
+let apellido= (prompt("Ingrese su Apellido"))
+let edad= Number(prompt("Ingrese su Edad"))
+
+let alerta = alert("Mi nombre es "+ nombre+" apellido "+apellido + " edad: "+edad )
+
+console.log("🚀 ~ file: main.js ~ line 3 ~ nombre", nombre)
