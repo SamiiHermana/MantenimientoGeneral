bc359b4261d8dc9e7f5ac95ea4fcf78c96796a93
shermana <shermana@holos.cl>
2021-07-17 21:55:06 -0300

    [JavaScriptCurso]Add Ajax, styles and form

--- main.js
+++ main.js
@@ -114,115 +114,262 @@
 //     li.innerHTML = alumno
 //     listado.appendChild(li);}
 
-
-
-
-
 //Desafío clases 9 y 10  Segunda entrega Proyecto Final
 
+// let boton = document.getElementById("agregar");
+
+// function resultadoAlumnos(alumno) {
+//   switch (alumno.nota) {
+//     case "1":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "2":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "3":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "4":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "5":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "6":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "7":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "8":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "9":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "10":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//   }
+// }
+
+// class Alumno {
+//   constructor(nombre, apellido, nota) {
+//     this.nombre = nombre;
+//     this.apellido = apellido;
+//     this.nota = nota;
+//     this.resultado = function () {
+//       console.log(
+//         `Alumno ${this.nombre} ${this.apellido}, obtuvo ${this.nota}`
+//       );
+//     };
+//   }
+// }
+// boton.onclick = () =>{
+//     let curso = [];
+//     let continuar = "";
+
+//     do {
+//       nombre = prompt("Ingrese Nombre del alumno");
+//       apellido = prompt("Ingrese Apellido del alumno");
+//       nota = prompt("Ingrese la nota de la asignatura");
+//       continuar = prompt(
+//         "¿Desea Continuar? Digite la letra C si la respuesta es si, de lo contrario presione cualquier otra letra"
+//       );
+//       let alumno = new Alumno(nombre, apellido, nota);
+
+//       curso.push(alumno);
+
+//       for (let i = 0; i < curso.length; i++) {
+//         const alumno = curso[i];
+//         if (alumno.nota > 10 || alumno.nota < 1) {
+//           alert("Ingrese una nota ente 1 y 10");
+//         } else {
+//           resultadoAlumnos(alumno);
+//         }
+//       }
+//       localStorage.setItem("curso", JSON.stringify(curso));
+//     } while (continuar === "C");
+
+//     let listado = document.getElementById("alumnos");
+
+//     for (const alumno of curso) {
+//       const alumnoJSON = JSON.stringify(alumno);
+//       let li = document.createElement("li");
+//       li.setAttribute("class", "alumnos");
+//       li.innerHTML = alumnoJSON;
+//       listado.appendChild(li);
+//     }
+//   }
 
 
-  let boton = document.getElementById("agregar");
+//Desafío clases 11 y 12
 
 
 
-function resultadoAlumnos(alumno) {
-    switch (alumno.nota) {
-      case "1":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
-        );
-        break;
-      case "2":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
-        );
-        break;
-      case "3":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
-        );
-        break;
-      case "4":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
-        );
-        break;
-      case "5":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
-        );
-        break;
-      case "6":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
-        );
-        break;
-      case "7":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
-        );
-        break;
-      case "8":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
-        );
-        break;
-      case "9":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
-        );
-        break;
-      case "10":
-        alert(
-          `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
-        );
-        break;
-    }
+// $("body").append('<button id="agregar">Agregar Alumno</button>');
+
+// function resultadoAlumnos(alumno) {
+//   switch (alumno.nota) {
+//     case "1":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "2":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "3":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "4":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "5":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "6":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Desaprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "7":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "8":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "9":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//     case "10":
+//       alert(
+//         `El alumno ${alumno.nombre}${alumno.apellido} está Aprobado con ${alumno.nota}`
+//       );
+//       break;
+//   }
+// }
+
+// class Alumno {
+//   constructor(nombre, apellido, nota) {
+//     this.nombre = nombre;
+//     this.apellido = apellido;
+//     this.nota = nota;
+//     this.resultado = function () {
+//       console.log(
+//         `Alumno ${this.nombre} ${this.apellido}, obtuvo ${this.nota}`
+//       );
+//     };
+//   }
+// }
+// $(document).ready(function () {
+//   $("button").click(function () {
+//     let curso = [];
+//     let continuar = "";
+
+//     do {
+//       nombre = prompt("Ingrese Nombre del alumno");
+//       apellido = prompt("Ingrese Apellido del alumno");
+//       nota = prompt("Ingrese la nota de la asignatura");
+//       continuar = prompt(
+//         "¿Desea Continuar? Digite la letra C si la respuesta es si, de lo contrario presione cualquier otra letra"
+//       );
+//       let alumno = new Alumno(nombre, apellido, nota);
+
+//       curso.push(alumno);
+
+//       for (let i = 0; i < curso.length; i++) {
+//         const alumno = curso[i];
+//         if (alumno.nota > 10 || alumno.nota < 1) {
+//           alert("Ingrese una nota ente 1 y 10");
+//         } else {
+//           resultadoAlumnos(alumno);
+//         }
+//       }
+//       localStorage.setItem("curso", JSON.stringify(curso));
+//     } while (continuar === "C");
+
+//     let listado = document.getElementById("alumnos");
+
+//     for (const alumno of curso) {
+//       const alumnoJSON = JSON.stringify(alumno);
+//       let li = document.createElement("li");
+//       li.setAttribute("class", "alumnos");
+//       li.innerHTML = alumnoJSON;
+//       listado.appendChild(li);
+//     }
+//   });
+// });
+
+
+//Desafío clases 11 y 12
+
+
+function submitStudentData() {
+
+  var name = document.getElementById("name").value;
+  var subject = document.getElementById("subject").value;
+  var book = document.getElementById("book").value;
+  var nota1 = document.getElementById("nota1").value;
+  var exam = document.getElementById("exam").value;
+  var nota2 = document.getElementById("nota2").value;
+
+ 
+
+  let  dataString = {
+
+  name: name,
+  subject: subject,
+  book:book,
+  nota1:nota1,
+  exam:exam,
+  nota2:nota2
   }
 
 
-class Alumno {
-  constructor(nombre, apellido, nota) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.nota = nota;
-    this.resultado = function () {
-      console.log(`Alumno ${this.nombre} ${this.apellido}, obtuvo ${this.nota}`);
-    };
-  }
-}
-boton.onclick = () =>{
-let curso = [];
-let continuar="";
-
-do {
-  nombre = prompt("Ingrese Nombre del alumno");
-  apellido = prompt("Ingrese Apellido del alumno");
-  nota = prompt("Ingrese la nota de la asignatura");
-  continuar=prompt("¿Desea Continuar? Digite la letra C si la respuesta es si, de lo contrario presione cualquier otra letra");
-  let alumno = new Alumno(nombre, apellido, nota);
-
-  curso.push(alumno);
-
-
-for (let i = 0; i < curso.length; i++) {
-  const alumno = curso[i];
-  if (alumno.nota > 10 || alumno.nota < 1) {
-    alert("Ingrese una nota ente 1 y 10");
+  if (name == '' || subject == '' || book == '' || nota1 == ''|| exam == ''|| nota2 == '') {
+  alert("Por favor, complete todos los campos");
   } else {
-    resultadoAlumnos(alumno);
+  $.ajax({
+  method: "POST",
+  url: "data.json",
+  data: dataString,
+
+  });
   }
-}
-localStorage.setItem("curso",JSON.stringify(curso));
-} while (continuar === "C" );
-
-let listado = document.getElementById("alumnos");
-
-for (const alumno of curso) {
-    const alumnoJSON= JSON.stringify(alumno);
-    let li = document.createElement("li");
-    li.setAttribute("class", "alumnos");
-    li.innerHTML = alumnoJSON;
-    listado.appendChild(li);}
-}
+  return false;
+  }
